81a5488fedfa8f7bd5767cb2741c3aab105c6f44
Ilya <iliya.pisligin@ma.ru>
2023-10-12 11:47:10 +0300

    init

---
+++ demo/src/demo1/DemoCounter.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="counter">
+    <counter-button @action="decrement"> - </counter-button>
+    <counter-value :value="count" />
+    <counter-button @action="increment"> + </counter-button>
+  </div>
+</template>
+
+<script setup>
+  import { ref } from 'vue';
+  import CounterButton from "@/demo1/CounterButton.vue";
+  import CounterValue from "@/demo1/CounterValue.vue";
+
+  const count = ref(0)
+
+  const decrement = () => {
+    count.value = count.value - 1
+  }
+
+  const increment = () => {
+    count.value = count.value + 1
+  }
+</script>
+
+<style lang="scss" scoped>
+  .counter {
+    display: flex;
+    align-items: center;
+  }
+</style>
--- demo/src/routes.js
+++ demo/src/routes.js
@@ -1,1 +1,12 @@
-export default []
+export default [
+    {
+        name: 'Counter Base',
+        path: '/counter-0',
+        component: () => import("./demo0/DemoCounter.vue")
+    },
+    {
+        name: 'Counter Base1',
+        path: '/counter-1',
+        component: () => import("./demo1/DemoCounter.vue")
+    }
+]
